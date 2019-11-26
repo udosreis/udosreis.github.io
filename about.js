2a39e0ea95aa9fbfa13cef7e05d99e077fd32c42
Ugo Dos Reis <ugo.dosreis@gmail.com>
2019-11-25 23:29:56 -0500

    Animated the header

--- about.js
+++ about.js
@@ -1,1 +1,19 @@
-
+currentPage = 0
+function scrollPosition() {
+    var height = window.innerHeight
+    var distScrolled = document.body.scrollTop
+    if (distScrolled >= height) {
+        currentPage = 1
+        document.getElementById("name").style.color = "#2C2C2C"
+        document.getElementsByClassName("Logos")[0].style.fill = "#2C2C2C"
+        document.getElementsByClassName("Logos")[1].style.fill = "#2C2C2C"
+        document.getElementsByClassName("next-page")[0].style.opacity = 0.0
+    }
+    else {
+        currentPage = 0
+        document.getElementById("name").style.color = "#FFFFFF"
+        document.getElementsByClassName("Logos")[0].style.fill = "#FFFFFF"
+        document.getElementsByClassName("Logos")[1].style.fill = "#FFFFFF"
+        document.getElementsByClassName("next-page")[0].style.opacity = 1.0
+    }
+}
