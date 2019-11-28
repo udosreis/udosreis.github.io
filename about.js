a8a1ad81df864fd0944ffef647a8f57381988293
Ugo Dos Reis <ugo.dosreis@gmail.com>
2019-11-28 15:47:34 -0500

    Fixed the np-indicator

--- about.js
+++ about.js
@@ -7,13 +7,15 @@ function scrollPosition() {
         document.getElementById("name").style.color = "#2C2C2C"
         document.getElementsByClassName("Logos")[0].style.fill = "#2C2C2C"
         document.getElementsByClassName("Logos")[1].style.fill = "#2C2C2C"
-        document.getElementsByClassName("next-page")[0].style.opacity = 0.0
     }
     else {
         currentPage = 0
         document.getElementById("name").style.color = "#FFFFFF"
         document.getElementsByClassName("Logos")[0].style.fill = "#FFFFFF"
         document.getElementsByClassName("Logos")[1].style.fill = "#FFFFFF"
-        document.getElementsByClassName("next-page")[0].style.opacity = 1.0
+        document.getElementsByClassName("next-page")[0].style.display = "block"
+    }
+    if (distScrolled > 80) {
+        document.getElementsByClassName("next-page")[0].style.display = "none"
     }
 }
