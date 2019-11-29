47e962a028ef06b3a2eb631752d62c6e8bd35f41
Ugo Dos Reis <ugo.dosreis@gmail.com>
2019-11-28 23:57:16 -0500

    Starting the background animations

---
+++ background.js
@@ -0,0 +1,21 @@
+window.onload() = animBckg()
+
+func animBckg() {
+    var date = new Date();
+    var current_hour = date.getHours();
+    if current_hour < 5 {
+
+    }
+    else if current_hour < 7 {
+        
+    }
+    else if current_hour < 17 {
+        
+    }
+    else if current_hour < 19 {
+        
+    }
+    else {
+        
+    }
+}
