75d15b52441e7c0c02b3bb76b38f74fff7dd992d
Ugo Dos Reis <ugo.dosreis@gmail.com>
2019-11-29 22:10:27 -0500

    changed the background of about page

--- background.js
+++ background.js
@@ -4,18 +4,18 @@ func animBckg() {
     var date = new Date();
     var current_hour = date.getHours();
     if current_hour < 5 {
-
+        document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #081F40 59%, #041022 100%);"
     }
     else if current_hour < 7 {
-        
+        document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #081F40 59%, #041022 100%);"
     }
     else if current_hour < 17 {
-        
+        document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #1A7AFF 59%, #5893E4 100%);"
     }
     else if current_hour < 19 {
-        
+        document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #081F40 59%, #041022 100%);"
     }
     else {
-        
+        document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #081F40 59%, #041022 100%);"
     }
 }
