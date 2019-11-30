e6382d9fb1e55ef74f07d2fbbba0bafc5665a6b1
Ugo Dos Reis <ugo.dosreis@gmail.com>
2019-11-29 22:15:05 -0500

    Fixed errors

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 window.onload() = animBckg()
 
-func animBckg() {
+function animBckg() {
     var date = new Date();
     var current_hour = date.getHours();
-    if current_hour < 5 {
+    if (current_hour < 5) {
         document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #081F40 59%, #041022 100%);"
     }
-    else if current_hour < 7 {
+    else if (current_hour < 7) {
         document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #081F40 59%, #041022 100%);"
     }
-    else if current_hour < 17 {
+    else if (current_hour < 17) {
         document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #1A7AFF 59%, #5893E4 100%);"
     }
-    else if current_hour < 19 {
+    else if (current_hour < 19) {
         document.body.style.backgroundImage = "radial-gradient(closest-side at 42% 18%, #081F40 59%, #041022 100%);"
     }
     else {
