d3256ec8a2f25de7d0cdb06e1f74bce81d71b461
udosreis <ugo.dosreis@gmail.com>
2021-09-05 15:40:02 +0100

    Fixed visual bug

--- about.js
+++ about.js
@@ -12,12 +12,14 @@ function scrollPosition() {
         document.getElementById("name").style.color = "#2C2C2C"
         document.getElementsByClassName("Logos")[0].style.fill = "#2C2C2C"
         document.getElementsByClassName("Logos")[1].style.fill = "#2C2C2C"
+        document.getElementsByClassName("Logos")[2].style.fill = "#2C2C2C"
     }
     else {
         currentPage = 0
         document.getElementById("name").style.color = "#FFFFFF"
         document.getElementsByClassName("Logos")[0].style.fill = "#FFFFFF"
         document.getElementsByClassName("Logos")[1].style.fill = "#FFFFFF"
+        document.getElementsByClassName("Logos")[2].style.fill = "#FFFFFF"
         //document.getElementsByClassName("next-page")[0].style.display = "block"
     }
     // if (distScrolled > 80) {
